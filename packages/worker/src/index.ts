ecd6303e50e5ca9446aeee24a68baab8de3972e0
dominikwozniak <wozniakdominik7312@gmail.com>
2024-06-12 20:01:16 +0200

    Add hono to cf worker

--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -1,1 +1,18 @@
-export const name = "worker";
+import { Hono } from "hono";
+
+/*
+ * Define the environment variables that your application will use.
+ * Hono disallows the use interface for defining environment variables.
+ */
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+type Env = {
+  MY_VAR: string;
+};
+
+const app = new Hono<{ Bindings: Env }>();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono! 🚀");
+});
+
+export default app;
