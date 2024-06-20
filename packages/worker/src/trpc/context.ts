9506db52392b5f6e430f7c00b6dd2b3a78f7ba29
dominikwozniak <wozniakdominik7312@gmail.com>
2024-06-20 22:50:02 +0200

    Clean up trpc context

--- packages/worker/src/trpc/context.ts
+++ packages/worker/src/trpc/context.ts
@@ -11,23 +11,24 @@ export const createContext = async (
   const d1 = env.DB;
   const db = dbClient(d1);
 
+  const clerk = createClerkClient({
+    secretKey: env.CLERK_SECRET_KEY,
+  });
+
   const language = req.headers.get("content-language") ?? "en";
   // TODO: use in monitoring and logging
   const source = req.headers.get("x-trpc-source") ?? "unknown";
   console.log(">>> tRPC Request from", source);
 
-  const clerk = createClerkClient({
-    secretKey: env.CLERK_SECRET_KEY,
-  });
-  const token = req.headers.get("x-clerk-auth-token");
+  const authToken = req.headers.get("x-clerk-auth-token");
   const sessionId = req.headers.get("x-clerk-auth-session-id");
 
   return {
     db,
     language,
     userId:
-      sessionId && token
-        ? await clerk.sessions.verifySession(sessionId, token)
+      sessionId && authToken
+        ? await clerk.sessions.verifySession(sessionId, authToken)
         : null,
   };
 };
